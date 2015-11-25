2cfa8b0b759f4899331f0a80a7f76c6b104a40cd
Francesco <micheli@cern.ch>
2015-11-25 16:40:20 +0100

    small fix

--- runSplit.sh
+++ runSplit.sh
@@ -2,11 +2,11 @@
 
 run=$1
 
-for i in {1..9..1}
+for i in {1..10..1}
 do
     cp test/configPhysicsDummySplit.dat test/configPhysicsDummySplit_"$run"_$i.dat
     cp data/physicsRunsDummySplit.dat data/physicsRunsDummySplit_"$run"_$i.dat
-    sed -i "s|3990|$run\_1|" test/configPhysicsDummySplit_"$run"_$i.dat
+    sed -i "s|3990|$run\_$i|g" test/configPhysicsDummySplit_"$run"_$i.dat
     sed -i "s|3990|$run|" data/physicsRunsDummySplit_"$run"_$i.dat
     sed -i "s|*.root|$i*.root|" data/physicsRunsDummySplit_"$run"_$i.dat
     sed -i "s|physicsRunsDummySplit|physicsRunsDummySplit_"$run"_$i|" test/configPhysicsDummySplit_"$run"_$i.dat
