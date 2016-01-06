b6d18fe682ffd129dd627ef6e2654159f790c43d
Francesco <micheli@cern.ch>
2016-01-06 11:11:44 +0100

    small fix

--- runSplit.sh
+++ runSplit.sh
@@ -4,7 +4,7 @@ run=$1
 destinationdir="srm://t3se01.psi.ch/pnfs/psi.ch/cms/trivcat/store/user/micheli/rawData/"
 echo $destinationdir
 
-for i in {1..10..1}
+for i in {1..9..1}
 do
     cp test/configPhysicsDummySplit.dat test/configPhysicsDummySplit_"$run"_$i.dat
     cp data/physicsRunsDummySplit.dat data/physicsRunsDummySplit_"$run"_$i.dat
@@ -18,3 +18,4 @@ done
 hadd output_run$run.root output_run"$run"_*.root
 rm output_run"$run"_*.root
 gfal-copy file:///shome/micheli/H4Analysis/output_run$run.root $destinationdir
+rm output_run$run.root
